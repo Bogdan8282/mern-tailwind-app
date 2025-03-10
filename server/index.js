96fd7e54a4f45f7245d3735f37ca2eb4f212daf8
Богдан Матрофайло <144223853+Bogdan8282@users.noreply.github.com>
2025-03-10 13:07:03 +0200

    Minor improvements

--- server/index.js
+++ server/index.js
@@ -1,27 +1,25 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./db");
+const routes = require("./routes");
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: "http://localhost:5173/",
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use("/", routes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+// Підключення до бази даних
+connectDB();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Server is running" });
-});
-
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
